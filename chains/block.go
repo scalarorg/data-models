218478ff45b9c5f35d28eb91e9b37c4eb4a6a934
Vu Viet Tai <vuviettai@gmail.com>
2025-05-20 09:54:12 +0700

    Add unique index for block header

--- chains/block.go
+++ chains/block.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type BlockHeader struct {
 	gorm.Model
-	Chain       string `gorm:"type:varchar(255)"`
-	BlockNumber uint64 `gorm:"type:bigint"`
+	Chain       string `gorm:"type:varchar(255);index:idx_chain_blocknum,unique"`
+	BlockNumber uint64 `gorm:"type:bigint;index:idx_chain_blocknum,unique"`
 	BlockHash   string `gorm:"type:varchar(255)"`
 	BlockTime   uint64 `gorm:"type:bigint"`
 }
