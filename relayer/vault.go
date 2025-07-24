e53369f46afd3b6d1379473d0ac64f8ed3a4fe97
Vu Viet Tai <vuviettai@gmail.com>
2025-07-24 16:03:02 +0700

    Store vault transaction in relayer db

--- relayer/vault.go
+++ relayer/vault.go
@@ -1,21 +1,28 @@
 package relayer
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 // VaultBlock represents the processing status of each Vault block in the relayer
-type VaultBlock struct {
+// type VaultBlock struct {
+// 	gorm.Model
+// 	BlockNumber      uint64     `gorm:"type:bigint;index:idx_vault_block_number,unique"`
+// 	BlockHash        string     `gorm:"type:varchar(255)"`
+// 	Chain            string     `gorm:"type:varchar(64)"`
+// 	Status           string     `gorm:"type:varchar(32);default:'pending'"` // pending, processing, completed, failed
+// 	ProcessedAt      *time.Time `gorm:"type:timestamp"`
+// 	CompletedAt      *time.Time `gorm:"type:timestamp"`
+// 	Error            string     `gorm:"type:text"`
+// 	TransactionCount int        `gorm:"type:int;default:0"`
+// 	ProcessedTxCount int        `gorm:"type:int;default:0"` // Number of transactions that have been processed
+// }
+
+type VaultTransaction struct {
 	gorm.Model
-	BlockNumber      uint64     `gorm:"type:bigint;index:idx_vault_block_number,unique"`
-	BlockHash        string     `gorm:"type:varchar(255)"`
-	Chain            string     `gorm:"type:varchar(64)"`
-	Status           string     `gorm:"type:varchar(32);default:'pending'"` // pending, processing, completed, failed
-	ProcessedAt      *time.Time `gorm:"type:timestamp"`
-	CompletedAt      *time.Time `gorm:"type:timestamp"`
-	Error            string     `gorm:"type:text"`
-	TransactionCount int        `gorm:"type:int;default:0"`
-	ProcessedTxCount int        `gorm:"type:int;default:0"` // Number of transactions that have been processed
+	BlockHeight uint64 `gorm:"type:bigint;index:idx_vault_transaction_block_position,unique"`
+	TxPosition  uint   `gorm:"type:bigint;default:0;index:idx_vault_transaction_block_position,unique"`
+	TxHash      string `gorm:"type:varchar(128)"`
+	Chain       string `gorm:"type:varchar(64)"`
+	Status      string `gorm:"type:varchar(32);default:'pending'"` // pending, processing, completed, failed
 }
